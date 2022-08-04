7c14ecc9c109deca35063b6561a605bceacf69e2
Javlon Fattoev <javlonfattoev@gmail.com>
2022-08-04 21:10:40 +0500

    operators

---
+++ Lesson3/Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿bool a = true;
+bool b = !a;
+
+if(b) // b ni qiymati endi false
+    Console.WriteLine("True");
+
+bool c = a || b;
+
+// 1 || 1 = 1
+// 1 || 0 = 1
+// 0 || 1 = 1
+// 0 || 0 = 0
+
+c = a && b;
+
+// 1 || 1 = 1
+// 1 || 0 = 0
+// 0 || 1 = 0
+// 0 || 0 = 0
+
+c = !c || !a && b || (a && b);
+
+Console.WriteLine(c);
+
+int t = 9;
+if(t == 6 && (t == int.Parse(null) && t == 3))
+    Console.WriteLine(t);
