90b282ec105010a8947ff92fa829e1852fa3d179
Javlon Fattoev <javlonfattoev@gmail.com>
2022-08-11 19:07:46 +0500

    lesson8

--- Lesson8/Arrays/Program.cs
+++ Lesson8/Arrays/Program.cs
@@ -8,9 +8,9 @@ string[] str = new string[2];
 string[] str2 = new string[] { "satr", "soz", "gap" };
 string[] str3 = { "satr", "soz", "gap" };
 
-var satr = str2[3]; // => ???
+//var satr = str2[3]; // => ???
 var sat1r = str2[2]; // => gap
 var s = str2[ms2[1]]; // => gap
-var a = str2[ms1[ms2[1]]]; // =>
+//var a = str2[ms1[ms2[1]]]; // => ???
 var b = str2[1]; // => soz
 
